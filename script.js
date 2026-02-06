a75665a75266143754b7dd300d1e691b8e33e531
Pernille00 <pernille.brunhoej@gmail.com>
2026-02-06 15:06:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+      function changeBackgroundColour(){
+            document.getElementsByClassName('list')[0].classList.toggle('lightColor');
+
+            }
+
+        function hideList(){
+            document.getElementsByClassName('list')[0].classList.toggle('hidden');
+        }
